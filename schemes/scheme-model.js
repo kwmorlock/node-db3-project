e0475949f5a90e4f85d4b788a5744585b9ebb244
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-13 17:06:16 -0400

    scheme-model finished

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -8,3 +8,39 @@ module.exports = {
   update,
   remove,
 };
+
+function find(){
+    return db('schemes')
+}
+
+function findById(id){
+    return db('schemes').where({ id }).first();
+}
+
+function findSteps(id){
+    return db('schemes')
+    .join("steps", "steps.scehme_id", "scheme.id" )
+    .select("steps.id", "schemes.scheme_name", "steps.step_number", "steps.instructions")
+    .where("schemes.id", id)
+    .orderBy("steps.step_number")
+
+}
+
+
+function add(scheme){
+return db("schemes")
+.insert(scheme)
+
+}
+
+function update(change, id){
+    return db('schemes')
+    .where({ id })
+    .update(change)
+}
+
+function remove(id){
+    return db('schemes')
+    .where({ id })
+    .del();
+}
--- server.js
+++ server.js
@@ -6,5 +6,6 @@ const server = express();
 
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
+//line 9 is starting point for site to use in postman
 
 module.exports = server;
