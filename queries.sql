f83497c4cc98c89ee315ee6c11bcfdeab09f39e9
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-13 18:56:14 -0400

    queries done

--- queries.sql
+++ queries.sql
@@ -2,12 +2,33 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
-select d.ProductName, d.CategoryId
-from employees as e
-join departments as d
+select p.ProductName, c.CategoryName
+from product as p
+join category as c 
+on p.CategoryId = c.Id
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+select o.Id, s.CompanyName
+from [Order] o 
+join Shipper as s 
+on o.ShipVia = s.Id 
+where o.OrderDate < '2012-08-09'; 
+
+
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+select p.ProductName, p.QuantityPerUnit
+from Product as p 
+join [Order] as o 
+ on p.SupplierId = o.ShipVia
+ where o.id = 10251
+ order by p.ProductName
+
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+select o.id, c.CompanyName, e.LastName
+from [Order] as o 
+join Customer as c 
+on o.CustomerId = c.id 
+join Employee as e 
+on o.EmployeeId = e.id
